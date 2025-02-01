7193d7983656ad125cb11b113f557274352da42a
Sassan Ghazi <117306645+sassanghz@users.noreply.github.com>
2025-02-01 17:16:17 -0500

    Delete frontend directory

--- frontend/scripts/map.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2Fzc2FuZ2h6IiwiYSI6ImNtNm1ubXcyYTBsbWYybnE0aWF0NnkwNmkifQ.nRXv8m0gpOWHiaK4JqsSxw';
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [0, 0],
-    zoom: 1
-});
